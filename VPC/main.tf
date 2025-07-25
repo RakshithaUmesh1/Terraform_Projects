fc1969d8f685747e2f50f7053c2f1218c422f91e
Rakshitha Umesh <159748251+RakshithaUmesh1@users.noreply.github.com>
2025-07-25 13:06:21 +0530

    cidr block set

--- VPC/main.tf
+++ VPC/main.tf
@@ -4,7 +4,7 @@ provider "aws"{
      secret_key="pzY5nq1qyYNv0CPAeZ1CS3t0mpSEoE7tVr0fcc8B"
  }
 resource "aws_vpc" "myvpc"{
-       cidr_block="0.0.0.0/16"
+       cidr_block="10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = {
