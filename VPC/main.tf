f75c93285e47b0d4908c76140414723139159ca4
Rakshitha Umesh <159748251+RakshithaUmesh1@users.noreply.github.com>
2025-07-25 13:04:06 +0530

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -5,7 +5,6 @@ provider "aws"{
  }
 resource "aws_vpc" "myvpc"{
        cidr_block="0.0.0.0/16"
-       cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = {
