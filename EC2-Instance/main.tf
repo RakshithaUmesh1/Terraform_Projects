9e41fd8db637ce1e716764506e0a29e876e5ad0a
Rakshitha Umesh <159748251+RakshithaUmesh1@users.noreply.github.com>
2025-07-25 16:33:02 +0530

    security_group added

--- EC2-Instance/main.tf
+++ EC2-Instance/main.tf
@@ -67,6 +67,7 @@ resource "aws_instance" "ec2" {
     subnet_id=aws-subnet.student-1.id
     ami="ami-0d0ad8bb301edb745"
     instance_type="t2.micro"
+    security_groups= [ "default" ]
     key_name="new-key"
    root_block_device{
    volume_size=20
